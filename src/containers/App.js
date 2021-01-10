175354bc4f7525f28f2f7b2c291647a17e292a52
abhayy947 <71834710+abhayy947@users.noreply.github.com>
2021-01-10 12:03:03 +0530

    second

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,35 +18,35 @@ import Scroll from '../components/Scroll';
 		    fetch('http://jsonplaceholder.typicode.com/users')
 				    .then(response=>response.json())
 				    .then(users=>this.setState({robots:users}));
-				}
+		 }
 
 
- 	        onSearchChange=(event)=>{
-  	            this.setState({searchField:event.target.value});
-        }
+ 	    onSearchChange=(event)=>{
+  	        this.setState({searchField:event.target.value});
+         }
 
     
         render(){
         	const {robots,searchField}=this.state;
-		    const filteredrobots=robots.filter(robot=>{
-		  			     return robot.name.toLowerCase().includes(searchField.toLowerCase())
-		    });
+		    const filteredrobots=robots.filter(robot=>
+		  			      robot.name.toLowerCase().includes(searchField.toLowerCase())
+		    );
 		 		
-		 	if(!robots.length){
-		 		return(<h1>Loading</h1>)}else{
-                return  (
-				          <div className='tc'>
-				               <h1 className='f1'>ROBOFRIENDS</h1>
-				               <SearchBox searchChange={this.onSearchChange}/>
-				     	          <Scroll>
-				     	             <ErrorBoundry>
+		 	return !robots.length ?
+		 		<h1>Loading</h1>
+		 		: 
+				    <div className='tc'>
+				        <h1 className='f1'>ROBOFRIENDS</h1>
+				            <SearchBox searchChange={this.onSearchChange}/>
+				     	        <Scroll>
+				     	            <ErrorBoundry>
 				     	              <CardList robots={filteredrobots}/>
-				     	              </ErrorBoundry>
-				     	          </Scroll>
-				     	      </div>
-				     	)
-			}
+				     	             /ErrorBoundry>
+				     	        </Scroll>
+				     	    </div>
+				   ;
+			
 		}
-	}
+}
 
 export default App;
